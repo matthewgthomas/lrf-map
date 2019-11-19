97e915fc641901671094c884058cded13ae49e8b
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2019-11-19 17:04:36 +0000

    Adding lookup tables and filenames/URLs

---
+++ urls and filenames.r
@@ -0,0 +1,35 @@
+##
+## Local Resilience Forums - England ang Wales
+##
+# Local Resilience Forums (December 2017) England & Wales - Ultra-generalised
+url_ew_lo = "https://opendata.arcgis.com/datasets/1d510b7fd2d145de9b5665c06c4cfc25_4.geojson"
+
+# Local Resilience Forums (December 2017) England & Wales - Full resolution - extent of the realm
+url_ew_hi = "https://opendata.arcgis.com/datasets/1d510b7fd2d145de9b5665c06c4cfc25_1.geojson"
+
+
+##
+## Local Authorities / Local Government Districts
+## (for Scotland and Northern Ireland - who don't publish shapefiles for their Local Resilience Partnerships / Emergency Preparedness Groups)
+##
+# Local Authority Districts (April 2019) Boundaries UK - Full resolution - extent of the realm
+url_lad_hi = "https://opendata.arcgis.com/datasets/2b95585accc4437b97d766f31c5568cb_0.geojson"
+
+# Local Authority Districts (April 2019) Boundaries UK - Ultra-generalised
+url_lad_lo = "https://opendata.arcgis.com/datasets/bbb0e58b0be64cc1a1460aa69e33678f_0.geojson"
+
+# OSNI Open Data Largescale Boundaries - Local Government Districts 2012
+# (note there's only the one file - not separate high and low res versions)
+url_lgd = "http://osni-spatial-ni.opendata.arcgis.com/datasets/a55726475f1b460c927d1816ffde6c72_2.geojson"
+
+
+##
+## lookups
+##
+file_sco_lad_lrp_lookup = "Scotland Local Authority to Local Resilience Partnership lookup.csv"
+
+file_ni_lgd_epg_lookup = "Northern Ireland Local Government District to Emergency Preparedness Group lookup.csv"
+
+
+# name of file(s) to save
+output_file = "LRFs_and_EPGs_and_LRPs"  # we'll append "low/high res" and file extensions later
